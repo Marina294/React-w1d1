1cf9e17a66aab28e21822020180cd544d8adf189
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-03-08 19:30:09 -0800

    first commit

---
+++ 06-practice-class/Category.js
@@ -0,0 +1,41 @@
+// change this Component to a class!
+class Category extends React.Component {
+    constructor() {
+        super()
+    }
+    // get data from props and use map to build an array of <li>
+    // ... code goes here
+    render() {
+        const navibar = data.map((result, index) => (
+            <li className="cat-link left valign-wrapper" key={index.toString()}>
+                <i className="material-icons">{result.icon}</i>
+                {result.title}
+            </li>
+        ))
+
+        return(
+                /* Your Code Here */
+            <div className="row">
+                <ul className="cat-nav center-align">
+                    {navibar}
+                </ul>   
+            </div>
+        )
+    }
+}
+
+
+
+// let navibar = data.map((result, index) => {
+//     console.log(result)
+//     return(
+//       <Category key={index} data={result} />
+//   )})
+  
+//   ReactDOM.render(
+//       <div className="row">
+//         {navibar}
+//       </div>,
+//     document.getElementById('root')
+//   );
+  
